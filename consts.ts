f125ad81b8b52c38649a578e5004120bb23c37a3
sula-frontend <sula7ne@gmail.com>
2025-07-01 19:57:29 +0200

    constant values

---
+++ consts.ts
@@ -0,0 +1,12 @@
+import { avnu } from "./starknet/defies/avnu";
+import { jediSwap } from "./starknet/defies/jediSwap";
+import { tenKSwap } from "./starknet/defies/tenKSwap";
+import { zkLend } from "./starknet/defies/zkLend";
+
+
+export const defiesConsts = {
+    'zkLend': zkLend,
+    'jediSwap': jediSwap,
+    'avnu': avnu,
+    'tenKSwap': tenKSwap
+}
